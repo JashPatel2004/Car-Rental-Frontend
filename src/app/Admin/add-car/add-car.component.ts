576d40c08ad9a5c56e3473de3c46b17b02b2ef13
JashPatel2004 <www.jashpatel211@gmail.com>
2024-07-26 00:14:10 +0530

    validation on add car component

--- src/app/Admin/add-car/add-car.component.ts
+++ src/app/Admin/add-car/add-car.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-add-car',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule,ReactiveFormsModule],
   templateUrl: './add-car.component.html',
   styleUrl: './add-car.component.css'
 })
@@ -13,6 +14,24 @@ export class AddCarComponent {
 
   imageUrl: string | ArrayBuffer | null | undefined = null;
   fileName:string='No file chosen'
+  AddCar:FormGroup
+  isValid:boolean=false
+
+  constructor(private formBuilder:FormBuilder){
+    this.AddCar=formBuilder.group({
+      image:['',[Validators.required]],
+      manufacturer:['',[Validators.required]],
+      model:['',[Validators.required]],
+      year:['',[Validators.required]],
+      bodytype:['',[Validators.required]],
+      mileage:['',[Validators.required]],
+      licenseplate:['',[Validators.required]],
+      transmission:['',[Validators.required]],
+      price:['',[Validators.required]],
+      seating:['',[Validators.required]],
+      description:['',[Validators.required]],
+    })
+  }
 
   onFileSelected(event:Event){
     const fileInput = event.target as HTMLInputElement;
